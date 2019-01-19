7706c97002bc755fb5b89de19c106f9d5ff3f342
Vamsi Batchu <vamsibatchuk@gmail.com>
2019-01-19 12:05:50 -0500

    Feature 2 Implemented
    Added parking lot , traffic signal violations.

--- app/.externalNativeBuild/cmake/release/x86_64/CMakeFiles/3.6.0-rc2/CMakeSystem.cmake
+++ app/.externalNativeBuild/cmake/release/x86_64/CMakeFiles/3.6.0-rc2/CMakeSystem.cmake
@@ -1,9 +1,9 @@
-set(CMAKE_HOST_SYSTEM "Darwin-17.7.0")
+set(CMAKE_HOST_SYSTEM "Darwin-18.0.0")
 set(CMAKE_HOST_SYSTEM_NAME "Darwin")
-set(CMAKE_HOST_SYSTEM_VERSION "17.7.0")
+set(CMAKE_HOST_SYSTEM_VERSION "18.0.0")
 set(CMAKE_HOST_SYSTEM_PROCESSOR "x86_64")
 
-include("/Users/nivedithag/Library/Android/sdk/ndk-bundle/build/cmake/android.toolchain.cmake")
+include("/Users/vamsi/Library/Android/sdk/ndk-bundle/build/cmake/android.toolchain.cmake")
 
 set(CMAKE_SYSTEM "Android-1")
 set(CMAKE_SYSTEM_NAME "Android")
--- app/src/main/cpp/samplearapp.cpp
+++ app/src/main/cpp/samplearapp.cpp
@@ -9,6 +9,12 @@
 #include <map>
 #include <cmath>
 #include <vector>
+#include <time.h>
+#include <iostream>
+#include <chrono>
+#include <ctime>
+
+
 #include "SpeedCheck.h"
 
 #define earthRadiusKm 6371.0
@@ -19,6 +25,16 @@ using namespace WayRay;
 
 class DetectionObjectListener : public Listener<DetectionObject> {
 public:
+
+    /*
+     * PARKING VARIABLES
+     */
+    int start_park_time = 0;
+
+    // Time checker
+    time_t my_time;
+
+
     DetectionObjectListener() {
     }
     double deg2rad(double deg){
@@ -29,6 +45,11 @@ public:
         return (deg*M_PI/180);
     }
 
+    static double now_ms(void) {
+        struct timespec res;
+        clock_gettime(CLOCK_REALTIME, &res);
+        return 1000.0 * res.tv_sec + (double) res.tv_nsec / 1e6;
+    }
 
     double distanceEarth(double lat1d, double lon1d, double lat2d, double lon2d) {
         double lat1r, lon1r, lat2r, lon2r, u, v;
@@ -42,14 +63,27 @@ public:
     }
 
     void changed(std::shared_ptr<DetectionObject> object) {
+
+        /* CHECK PARKING TIME
+         * TIME FOR PARKING DETECTION: 30 SECONDS (CAN BE ADJUSTED ACCORDING TO REQUIREMENTS)
+         */
+
+        if(start_park_time>30){
+            LOGI("Your in parking mode");
+        }
+
+        /* FEATURE 1 : TRAFFIC LIGHT VIOLATION CODE START:
+         *
+         */
+
         if (object->getType() == DetectionObject::Type::TrafficLight) {
             if(object->getState() == DetectionObject::TrafficLightState::Red){
                 double dist = distanceEarth(object->getPose().latitude,object->getPose().longitude,Context::get()->getVehicleState().getPose().latitude,Context::get()->getVehicleState().getPose().longitude);
                 //float distance_of_traffic_light_from_car = object->getDistance();
                 double speed_of_car = Context::get()->getVehicleState().getSpeed();
-                LOGI("Distance: %f",dist);
+                // LOGI("Distance: %f",dist);
                 if(dist<=0.5 && speed_of_car>=0.5){
-                    LOGI("You crossed the red light.");
+                    //LOGI("You crossed the red light.");
                 }
                 if (object->getStatus() == DetectionObject::Status::New || object->getStatus() == DetectionObject::Status::Changed || object->getStatus() == DetectionObject::Status::NotChanged) {
                     std::map<std::shared_ptr<DetectionObject>,std::shared_ptr<ARObject> >::iterator i = arrowMap.find(object);
@@ -85,8 +119,26 @@ public:
             }
 
         }
-    }
 
+        /*
+         FEATURE 2 : PARKING VIOLATION LOGIC
+         */
+
+        if (object->getType() != DetectionObject::Type::ParkingPlace && Context::get()->getVehicleState().getSpeed()==0){
+            my_time = time(NULL);
+            char* current_time = ctime(&my_time);
+            std::string str(current_time);
+            std::string seconds = str.substr(17,2);
+            start_park_time = std::stoi(seconds,nullptr,10);
+            //LOGI("%d", sec);
+        }else{
+            start_park_time=0;
+        }
+
+        /*
+         PARKING LOGIC ENDS
+         */
+    }
     std::map<std::shared_ptr<DetectionObject>, std::shared_ptr<ARObject> > arrowMap;
 
 };
@@ -98,13 +150,13 @@ class DetectionObjectSampleARApp: public Application {
         std::shared_ptr<CheckSpeedingListener> speedCheckListener = std::make_shared<CheckSpeedingListener>(CheckSpeedingListener(50));
         Context::get()->getScene().registerDetectionObjectListener(detectionObjectListener);
         Context::get()->getVehicleState().registerSpeedChangeListener(speedCheckListener);
-        LOGI("DetectionObjectListener registered.");
+        //LOGI("DetectionObjectListener registered.");
     }
 
     virtual void onStop() {
         Context::get()->getScene().unregisterDetectionObjectListener(detectionObjectListener);
         detectionObjectListener.reset();
-        LOGI("DetectionObjectListener unregistered.");
+        //LOGI("DetectionObjectListener unregistered.");
     }
 private:
     std::shared_ptr<DetectionObjectListener> detectionObjectListener;
